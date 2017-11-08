4f654c5873dacfb6fab58c0b22cf605831f6d954
Thibault <thibault.chevrin@navendis.com>
2017-11-08 08:34:44 +0100

    include changes #2 and atomicToPotentiality

--- src/data-flows/pipes.flow.ts
+++ src/data-flows/pipes.flow.ts
@@ -1,15 +1,24 @@
 import {
+	always,
+	and,
+	aperture,
+	applySpec,
 	converge,
 	curry,
 	has,
 	identity,
 	ifElse,
+	isNil,
 	maxBy,
 	partial,
+	path,
 	pipe,
 	prop,
 	reduce,
+	reject,
 	sortBy,
+	sum,
+	unapply,
 	unfold,
 	unnest,
 	without,
@@ -19,6 +28,7 @@ import { IConfig } from '../data-structures/config.interface';
 import { IMaterial } from '../data-structures/material.interface';
 import { IPotentiality } from '../data-structures/potentiality.interface';
 import { IPressureChunk } from '../data-structures/pressure-chunk.interface';
+import { RestrictionCondition } from '../data-structures/query.enum';
 import { IQuery, ITimeRestriction } from '../data-structures/query.interface';
 import { IRange } from '../data-structures/range.interface';
 
@@ -26,6 +36,7 @@ import { computePressureChunks } from './environment.flows';
 
 type maskFn = (tm: IRange) => IRange[];
 type mapRange = (r: IRange[], tm: IRange) => IRange[];
+type queryToPot = (q: IQuery) => IPotentiality[];
 
 export const queriesToPipeline = (config: IConfig, queries: IQuery[]): IMaterial[] => {
 	const potentials = queriesToPotentialities(config, queries);
@@ -57,7 +68,7 @@ const queriesToPotentialities = (config: IConfig, queries: IQuery[]): IPotential
 	return unnest(
 		queries.map(
 			converge(updateChildren, [
-				ifElse(has('goal'), goalToPotentiality, atomicToPotentiality),
+				ifElse(has('goal'), goalToPotentiality, atomicToPotentiality(config)),
 				queryToMask(config),
 			]),
 		),
@@ -91,27 +102,70 @@ const mapToHourRange = (ranges: IRange[], mask: IRange): IRange[] => {
 };
 
 const getMaskFilterFn = (tr: ITimeRestriction, mapFn: mapRange): maskFn => {
-	return pipe(
-		converge(maskRangeUnion, [
-			partial(mapFn, tr.ranges.map(r => ({ start: r[0], end: r[1] }))),
+	return converge(
+		(ranges: IRange[], mask: IRange) =>
+			tr.condition === RestrictionCondition.InRange ? ranges : outboundToInboud(ranges, mask),
+		[
+			converge(maskRangeUnion, [
+				partial(mapFn, tr.ranges.map(r => ({ start: r[0], end: r[1] }))),
+				identity,
+			]),
 			identity,
-		]),
-		// TODO: transform if condition === outrange
+		],
 	);
 };
 
+const outboundToInboud = (ranges: IRange[], mask: IRange): IRange[] => {
+	const prepRanges: IRange[] = [
+		{ start: -Infinity, end: mask.start },
+		...ranges,
+		{ start: mask.end, end: Infinity },
+	];
+	return reject<IRange | null>(
+		isNil,
+		aperture(2, prepRanges).map(
+			([r1, r2]) => (r1.end >= r2.start ? null : { start: r1.end, end: r2.start }),
+		),
+	) as IRange[];
+};
+
 const maskRangeUnion = (ranges: IRange[], mask: IRange): IRange[] => {
-	return [];
+	return ranges
+		.filter(r => r.start < mask.end && r.end > mask.start)
+		.sort((a, b) => a.start - b.start)
+		.map(range => ({
+			end: Math.min(mask.end, range.end),
+			start: Math.max(mask.start, range.start),
+		}));
 };
 
 const updateChildren = (potentials: IPotentiality[], mask: IRange[]): IPotentiality[] => {
-	return [];
+	return potentials.map(
+		pipe(
+			p => ({ ...p, children: maskRangeUnion(mask, p.children[0]) }),
+			p => ({ ...p, potentiel: p.duration / sum(p.children.map(c => c.end - c.start)) }),
+		),
+	);
 };
 
 const goalToPotentiality = (query: IQuery): IPotentiality[] => {
 	return [];
 };
 
-const atomicToPotentiality = (query: IQuery): IPotentiality[] => {
-	return [];
+const atomicToPotentiality = (config: IConfig) => {
+	const t = converge(
+		unapply(
+			applySpec<IPotentiality>({
+				children: path(['0', '1']),
+				duration: path(['0', '0']),
+				name: path(['1']),
+				potentiel: always(-1),
+			}),
+		),
+		[
+			ifElse(and(has('start'), has('end')), always([0, []]), always([0, []])),
+			(q: IQuery) => `${q.name}-atomic`,
+		],
+	);
+	return t;
 };
