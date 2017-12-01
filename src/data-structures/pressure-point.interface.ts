df1c53b89524f12c4b9ea2a2d8a40943d9c92ca9
Thibault <PBM42@users.noreply.github.com>
2017-12-01 13:50:45 +0100

    feat: add scheduler

---
+++ src/data-structures/pressure-point.interface.ts
@@ -0,0 +1,9 @@
+export interface IPressurePoint {
+	time: number;
+	pressureDiff: number;
+}
+
+export interface IPressureChunkPoint {
+	start: number;
+	pressure: number;
+}
---
+++ src/data-structures/query.enum.ts
@@ -0,0 +1,14 @@
+export enum QueryKind {
+  Placeholder,
+  Atomic
+}
+
+export enum GoalKind {
+  Atomic,
+  Splittable
+}
+
+export enum RestrictionCondition {
+  InRange,
+  OutRange
+}
