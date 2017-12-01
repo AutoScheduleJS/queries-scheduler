f6b80d2edd47a9f1a252ca516886dc0d362253b9
PBM42 <PBM764927@gmail.com>
2017-12-01 13:46:44 +0100

    update build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack');
 const path = require('path');
-const TypedocWebpackPlugin = require('typedoc-webpack-plugin');
 const nodeEnv = process.env.NODE_ENV || 'development';
 const isProd = nodeEnv === 'production';
 const rxPaths = require('rxjs/_esm5/path-mapping');
@@ -23,17 +22,15 @@ const config = {
 			},
 		}),
 		new webpack.optimize.ModuleConcatenationPlugin(),
-		new TypedocWebpackPlugin({
-			out: '../docs',
-			mode: 'modules',
-			module: 'commonjs',
-			target: 'ES2015',
-			name: 'Query Scheduler',
-			readme: '../README.md',
-		}),
 	],
 	module: {
-		loaders: [{ test: /\.ts$/, loader: 'ts-loader' }],
+		rules: [
+			{
+				test: /\.ts$/,
+				loader: 'ts-loader',
+				options: { onlyCompileBundledFiles: true, compilerOptions: { outDir: '.' } },
+			},
+		],
 	},
 };
 
